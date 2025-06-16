14cfcf165834b811dca5f941a9ce567f9671e256
Tobbe Lundberg <tobbe@tlundberg.com>
2025-06-16 15:52:47 +0200

    feat(vite): Use Vite for CedarJS's Cell transform (#124)
    Starting to move away from babel as its require hook, which is what we
    use, is not compatible with ESM imports.
    
    > Note: @babel/register does not support compiling native Node.js ES
    modules on the fly, since currently there is no stable API for
    intercepting ES modules loading.
    
    – _https://babel.dev/docs/babel-register_

--- packages/testing/config/jest/web/webBabelConfig.js
+++ packages/testing/config/jest/web/webBabelConfig.js
@@ -1,3 +1,18 @@
 const { getWebSideDefaultBabelConfig } = require('@cedarjs/babel-config')
 
-module.exports = getWebSideDefaultBabelConfig({ forJest: true })
+const redwoodCellsPlugin = require('../babelPlugins/babel-plugin-redwood-cell')
+
+const defaultWebSideBabelConfig = getWebSideDefaultBabelConfig({
+  forJest: true,
+})
+
+module.exports = {
+  ...defaultWebSideBabelConfig,
+  overrides: [
+    ...defaultWebSideBabelConfig.overrides,
+    {
+      test: /.+Cell.(js|tsx|jsx)$/,
+      plugins: [redwoodCellsPlugin],
+    },
+  ],
+}
