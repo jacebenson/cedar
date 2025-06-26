cd4dca415e98f5603962df3473bf4fe5486eefe9
Tobbe Lundberg <tobbe@tlundberg.com>
2025-06-27 01:44:56 +0200

    fix(api-server): handle path names with 'graphql' (#157)
    We have code in the api-server package that looks for all serverless
    functions and automatically loads them and adds them to the api server.
    We want the first function in the list to be the graphql function. But
    the code we had to find the graphql function looked at the entire path
    for the 'graphql' string. So if the project was placed in a parent
    folder that had 'graphql' in its name *all* functions would match that
    check.
    
    The updated code in this PR only looks at the file name, not the entire
    path.
    
    Fix ported over and improved from
    https://github.com/redwoodjs/graphql/pull/12055

--- packages/api-server/src/plugins/lambdaLoader.ts
+++ packages/api-server/src/plugins/lambdaLoader.ts
@@ -66,7 +66,7 @@ export const loadFunctionsFromDist = async (
   )
 
   // Place `GraphQL` serverless function at the start.
-  const i = serverFunctions.findIndex((x) => x.includes('graphql'))
+  const i = serverFunctions.findIndex((x) => path.basename(x) === 'graphql.js')
   if (i >= 0) {
     const graphQLFn = serverFunctions.splice(i, 1)[0]
     serverFunctions.unshift(graphQLFn)
