dfba15153453bd905b580b3295819d81e18fb05b
Tobbe Lundberg <tobbe@tlundberg.com>
2025-07-06 08:25:13 +0200

    chore(tests): Flush promises before running test (#174)
    Trying to work around this error in our CI
    
    
    ![image](https://github.com/user-attachments/assets/c4773b40-3d5f-4735-85f0-e8a7b62a9c63)
    
    Discussion here: https://github.com/vitest-dev/vitest/discussions/6511
    Original issue on vitest:
    https://github.com/vitest-dev/vitest/issues/6479

--- packages/cli/src/commands/setup/middleware/ogImage/__codemod_tests__/vitePlugin.ts
+++ packages/cli/src/commands/setup/middleware/ogImage/__codemod_tests__/vitePlugin.ts
@@ -1,6 +1,14 @@
-import { describe, it } from 'vitest'
+import { beforeAll, describe, it } from 'vitest'
 
 describe('Vite plugin codemod', () => {
+  beforeAll(async () => {
+    // Was running into this issue
+    // https://github.com/vitest-dev/vitest/discussions/6511
+    // One workaround that was posted there was this:
+    // TODO: Remove this workaround once the issue is fixed
+    await new Promise((res) => setImmediate(res))
+  })
+
   it('Handles the default vite config case', async () => {
     await matchTransformSnapshot('codemodVitePlugin', 'defaultViteConfig')
   })
