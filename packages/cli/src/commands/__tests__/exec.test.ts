2c78ac73f10c405ca9f87f51b8a391fbf31f75ce
Tobbe Lundberg <tobbe@tlundberg.com>
2025-07-08 10:04:07 +0200

    chore(cli): Reuse existing configureBabel() function in execHandler (#182)
    `lib/exec` already had an exported function that does exactly what I was
    doing in `execHandler.js`. So now I just use that function instead of
    duplicating the code. DRY!

--- packages/cli/src/commands/__tests__/exec.test.ts
+++ packages/cli/src/commands/__tests__/exec.test.ts
@@ -3,8 +3,10 @@ import path from 'node:path'
 import { fs as memfs, vol } from 'memfs'
 import { vi, afterEach, beforeEach, describe, it, expect } from 'vitest'
 
+// @ts-expect-error - No types for .js files
 import { runScriptFunction } from '../../lib/exec.js'
 import '../../lib/mockTelemetry'
+// @ts-expect-error - No types for .js files
 import { handler } from '../execHandler.js'
 
 vi.mock('@cedarjs/babel-config', () => ({
@@ -41,6 +43,7 @@ vi.mock('@cedarjs/internal/dist/files', () => ({
 
 vi.mock('../../lib/exec', () => ({
   runScriptFunction: vi.fn(),
+  configureBabel: vi.fn(),
 }))
 
 vi.mock('fs', () => ({ ...memfs, default: { ...memfs } }))
