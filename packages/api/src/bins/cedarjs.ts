41daa5730f1eb0f6fdab655472d0b1c0635fbf88
Tobbe Lundberg <tobbe@tlundberg.com>
2025-07-12 07:14:24 +0200

    feat(cli): cedarjs and cj bins (#195)
    Added `cedarjs` and `cj` bins in addition to the existing `redwood` and
    `rw` bins. So you can do things like `yarn cj info` or `yarn cj g page
    about`

---
+++ packages/api/src/bins/cedarjs.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+
+/**
+ * This file lets users run the Cedar CLI commands inside the /api directory
+ * in their projects.
+ * This works because of the "bin" field in the @cedarjs/api package.json file
+ * that points to this file.
+ */
+
+import { createRequire } from 'node:module'
+import { pathToFileURL } from 'node:url'
+
+const cliPackageJsonFileUrl = pathToFileURL(
+  require.resolve('@cedarjs/cli/package.json'),
+)
+
+const requireFromCli = createRequire(cliPackageJsonFileUrl)
+const bins = requireFromCli('./package.json')['bin']
+const cliEntryPointUrl = new URL(bins['cedarjs'], cliPackageJsonFileUrl)
+
+// If this is defined, we're running through yarn and need to change the cwd.
+// See https://yarnpkg.com/advanced/lifecycle-scripts/#environment-variables.
+if (process.env.PROJECT_CWD) {
+  process.chdir(process.env.PROJECT_CWD)
+}
+
+import(cliEntryPointUrl.toString())
