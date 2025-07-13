ceccf0a54480b2bd62892adc2d262ca8cceb0e60
Tobbe Lundberg <tobbe@tlundberg.com>
2025-07-13 22:09:55 +0200

    chore(cli): Workaround for codemod CI test failures (#204)
    Adding this workaround to more tests:
    https://github.com/cedarjs/cedar/pull/174

--- packages/cli/src/commands/setup/middleware/ogImage/__codemod_tests__/vitePlugin.ts
+++ packages/cli/src/commands/setup/middleware/ogImage/__codemod_tests__/vitePlugin.ts
@@ -4,6 +4,15 @@ describe('Vite plugin codemod', () => {
   beforeAll(async () => {
     // Was running into this issue
     // https://github.com/vitest-dev/vitest/discussions/6511
+    //   Error: [vitest-worker]: Timeout calling "onTaskUpdate"
+    // One workaround that was posted there was this:
+    // TODO: Remove this workaround once the issue is fixed
+    await new Promise((res) => setImmediate(res))
+  })
+
+  beforeAll(async () => {
+    // Was running into this issue
+    // https://github.com/vitest-dev/vitest/discussions/6511
     // One workaround that was posted there was this:
     // TODO: Remove this workaround once the issue is fixed
     await new Promise((res) => setImmediate(res))
--- packages/cli/src/commands/setup/uploads/__codemod_tests__/dbCodemod.test.ts
+++ packages/cli/src/commands/setup/uploads/__codemod_tests__/dbCodemod.test.ts
@@ -1,10 +1,19 @@
 import path from 'node:path'
 
-import { describe, it, expect } from 'vitest'
+import { beforeAll, describe, it, expect } from 'vitest'
 
 import { runTransform } from '../../../../lib/runTransform.js'
 
 describe('Db codemod', () => {
+  beforeAll(async () => {
+    // Was running into this issue
+    // https://github.com/vitest-dev/vitest/discussions/6511
+    //   Error: [vitest-worker]: Timeout calling "onTaskUpdate"
+    // One workaround that was posted there was this:
+    // TODO: Remove this workaround once the issue is fixed
+    await new Promise((res) => setImmediate(res))
+  })
+
   it('Handles the default db case', async () => {
     await matchTransformSnapshot('dbCodemod', 'defaultDb')
   })
