f070c491bfa9c47763be3c42ea7cd448c329a0c4
Tobbe Lundberg <tobbe@tlundberg.com>
2025-07-19 12:28:26 +0200

    improve regex

--- packages/testing/src/vitest/vite-plugin-create-auth-import-transform.ts
+++ packages/testing/src/vitest/vite-plugin-create-auth-import-transform.ts
@@ -9,8 +9,10 @@ export function createAuthImportTransformPlugin(): PluginOption {
         // Remove any existing import of `createAuth` without affecting anything else.
         // This regex defines 4 capture groups, where the second is `createAuth` and
         // the third is an (optional) comma for subsequent named imports — we want to remove those two.
+        // Using a negative lookbehind as recommended by
+        // https://codeql.github.com/codeql-query-help/javascript/js-polynomial-redos/
         code = code.replace(
-          /(import\s*{\s*[^}]*)(\bcreateAuth\b)(,?)([^}]*})/,
+          /(import\s*{(?<!\s)\s*[^}]*)(\bcreateAuth\b)(,?)([^}]*})/,
           '$1$4',
         )
         // Add import to mocked `createAuth` at the top of the file.
