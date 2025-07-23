33967ad63b2f8acb7beed93b805756e17bb18cac
Tobbe Lundberg <tobbe@tlundberg.com>
2025-07-23 07:12:27 +0200

    use mergeConfig() for vitest api side config

--- packages/testing/src/vitest/vite-plugin-cedar-api-vitest-config.ts
+++ packages/testing/src/vitest/vite-plugin-cedar-api-vitest-config.ts
@@ -1,5 +1,6 @@
 import path from 'node:path'
 
+import { mergeConfig } from 'vite'
 import type { Plugin } from 'vite'
 
 import { getPaths } from '@cedarjs/project-config'
@@ -9,51 +10,76 @@ export function cedarApiVitestConfigPlugin(): Plugin {
   return {
     name: 'cedar-vitest-plugin',
     config: (userConfig) => {
-      userConfig.define = {
-        ...getEnvVarDefinitions(),
-        ...userConfig.define,
-      }
+      // userConfig.define = {
+      //   ...getEnvVarDefinitions(),
+      //   ...userConfig.define,
+      // }
 
-      let userNoExternal = userConfig.ssr?.noExternal ?? []
-      if (!Array.isArray(userNoExternal)) {
-        userNoExternal = []
-      }
+      // let userNoExternal = userConfig.ssr?.noExternal ?? []
+      // if (!Array.isArray(userNoExternal)) {
+      //   userNoExternal = []
+      // }
 
-      // This is best-effort. If the user has noExternal configured with
-      // something that's not an array this might not behave as they expect.
-      // But this has to be good enough for now.
-      userConfig.ssr = {
-        ...userConfig.ssr,
-        noExternal: ['@cedarjs/testing', ...userNoExternal],
-      }
+      // // This is best-effort. If the user has noExternal configured with
+      // // something that's not an array this might not behave as they expect.
+      // // But this has to be good enough for now.
+      // userConfig.ssr = {
+      //   ...userConfig.ssr,
+      //   noExternal: ['@cedarjs/testing', ...userNoExternal],
+      // }
 
-      // TODO: Merge with userConfig.resolve.alias
-      userConfig.resolve = {
-        alias: {
-          src: getPaths().api.src,
+      // // TODO: Merge with userConfig.resolve.alias
+      // userConfig.resolve = {
+      //   alias: {
+      //     src: getPaths().api.src,
+      //   },
+      // }
+
+      // let userSetupFiles = userConfig?.test?.setupFiles ?? []
+      // if (!Array.isArray(userSetupFiles)) {
+      //   userSetupFiles = [userSetupFiles]
+      // }
+
+      // userConfig.test = {
+      //   environment: path.join(import.meta.dirname, 'CedarApiVitestEnv.js'),
+      //   // fileParallelism: false,
+      //   // fileParallelism doesn't work with vitest projects (which is what we're
+      //   // using in the root vitest.config.ts). As a workaround we set poolOptions
+      //   // instead, which also shouldn't work, but was suggested by Vitest team
+      //   // member AriPerkkio (Hiroshi's answer didn't work).
+      //   // https://github.com/vitest-dev/vitest/discussions/7416
+      //   poolOptions: { forks: { singleFork: true } },
+      //   ...userConfig.test,
+      //   setupFiles: [
+      //     path.join(import.meta.dirname, 'vitest-api.setup.js'),
+      //     ...userSetupFiles,
+      //   ],
+      // }
+
+      const cedarConfig = {
+        define: getEnvVarDefinitions(),
+        ssr: {
+          noExternal: ['@cedarjs/testing'],
+        },
+        resolve: {
+          alias: {
+            src: getPaths().api.src,
+          },
+        },
+        test: {
+          environment: path.join(import.meta.dirname, 'CedarApiVitestEnv.js'),
+          // fileParallelism: false,
+          // fileParallelism doesn't work with vitest projects (which is what we're
+          // using in the root vitest.config.ts). As a workaround we set poolOptions
+          // instead, which also shouldn't work, but was suggested by Vitest team
+          // member AriPerkkio (Hiroshi's answer didn't work).
+          // https://github.com/vitest-dev/vitest/discussions/7416
+          poolOptions: { forks: { singleFork: true } },
+          setupFiles: [path.join(import.meta.dirname, 'vitest-api.setup.js')],
         },
       }
 
-      const existingSetupFiles = userConfig?.test?.setupFiles ?? []
-      const existingSetupFilesArray = Array.isArray(existingSetupFiles)
-        ? existingSetupFiles
-        : [existingSetupFiles]
-
-      userConfig.test = {
-        environment: path.join(import.meta.dirname, 'CedarApiVitestEnv.js'),
-        // fileParallelism: false,
-        // fileParallelism doesn't work with vitest projects (which is what we're
-        // using in the root vitest.config.ts). As a workaround we set poolOptions
-        // instead, which also shouldn't work, but was suggested by Vitest team
-        // member AriPerkkio (Hiroshi's answer didn't work).
-        // https://github.com/vitest-dev/vitest/discussions/7416
-        poolOptions: { forks: { singleFork: true } },
-        ...userConfig.test,
-        setupFiles: [
-          path.join(import.meta.dirname, 'vitest-api.setup.js'),
-          ...existingSetupFilesArray,
-        ],
-      }
+      return mergeConfig(cedarConfig, userConfig)
     },
   }
 }
