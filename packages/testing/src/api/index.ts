5b609888dff7599af7654819fa6039d38679f32a
Tobbe Lundberg <tobbe@tlundberg.com>
2025-07-23 08:07:33 +0200

    export vitest preset from /api

--- packages/testing/src/api/index.ts
+++ packages/testing/src/api/index.ts
@@ -1,3 +1,4 @@
 export * from './apiFunction.js'
 export * from './scenario.js'
 export * from './directive.js'
+export { cedarApiVitestPreset } from '../vitest/vite-plugin-cedar-api-vitest-preset.js'
