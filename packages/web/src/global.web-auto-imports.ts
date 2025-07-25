8938d46d1f354cd1d214301cf36fa681f251bff5
Tobbe Lundberg <tobbe@tlundberg.com>
2025-07-26 01:38:22 +0200

    fix gql import path

--- packages/web/src/global.web-auto-imports.ts
+++ packages/web/src/global.web-auto-imports.ts
@@ -9,7 +9,7 @@ declare global {
   // This type is used for both regular RW projects and projects that have
   // enabled Trusted Documents. For regular RW projects, this could have been
   // typed just by importing gql from `graphql-tag`. But for Trusted Documents
-  // the type should be imported from `web/src/graphql/gql` in the user's
+  // the type should be imported from `web/src/graphql/gql.js` in the user's
   // project. The type here is generic enough to cover both cases.
   const gql: (
     source: string | TemplateStringsArray | readonly string[],
