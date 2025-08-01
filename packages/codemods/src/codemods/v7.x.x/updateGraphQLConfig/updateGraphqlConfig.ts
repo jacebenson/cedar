c6e047c5decc8671c89e8b3c3ef2aeb92a4530da
Tobbe Lundberg <tobbe@tlundberg.com>
2025-08-02 00:51:09 +0200

    fix(codemods): Link to git tag on GitHub for graphql config download (#325)

--- packages/codemods/src/codemods/v7.x.x/updateGraphQLConfig/updateGraphqlConfig.ts
+++ packages/codemods/src/codemods/v7.x.x/updateGraphQLConfig/updateGraphqlConfig.ts
@@ -7,9 +7,6 @@ import { getPaths } from '@cedarjs/project-config'
 
 export const updateGraphqlConfig = async () => {
   const res = await fetch(
-    // TODO: Have to come back here to update the URL when we have a more
-    // stable location than main
-    // 'https://raw.githubusercontent.com/redwoodjs/redwood/release/major/v7.0.0/packages/create-cedar-app/templates/ts/graphql.config.js'
     'https://raw.githubusercontent.com/cedarjs/cedar/refs/tags/v0.4.0/packages/create-cedar-app/templates/ts/graphql.config.js',
   )
   const text = await res.text()
