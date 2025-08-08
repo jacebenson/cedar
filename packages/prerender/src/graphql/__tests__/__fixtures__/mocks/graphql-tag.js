663240de43dd30a52a84576344bda939fedb266c
Tobbe Lundberg <tobbe@tlundberg.com>
2025-08-08 08:28:07 +0200

    Remove files deleted upstream

--- packages/prerender/src/graphql/__tests__/__fixtures__/mocks/graphql-tag.js
+++
@@ -1,9 +0,0 @@
-export const gql = (strings, ...values) => {
-  const query = strings.reduce((result, cur, i) => {
-    return result + cur + (values[i] || '')
-  }, '')
-
-  return { query, __isGqlTemplate: true }
-}
-
-export default gql
--- packages/prerender/src/graphql/__tests__/__fixtures__/node-runner/cedar-app/api/src/functions/index.js
+++
@@ -1,7 +0,0 @@
-// Index file for functions directory
-// This enables named imports like: import { graphqlHandler } from 'src/functions'
-
-export { default as graphqlHandler } from './graphql.js'
-
-// Re-export all named exports from individual function files
-export * from './graphql.js'
