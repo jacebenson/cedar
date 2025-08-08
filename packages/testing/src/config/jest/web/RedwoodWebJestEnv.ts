e28fa43b9cbfe66a69ee4703e5b7012c2eaffe12
Tobbe Lundberg <tobbe@tlundberg.com>
2025-08-08 10:02:26 +0200

    chore(testing): Remove an `any` from test setup

--- packages/testing/src/config/jest/web/RedwoodWebJestEnv.ts
+++ packages/testing/src/config/jest/web/RedwoodWebJestEnv.ts
@@ -5,11 +5,12 @@ import { TestEnvironment } from 'jest-environment-jsdom'
 // Due to issue: https://github.com/jsdom/jsdom/issues/2524
 // Fix from: https://github.com/jsdom/jsdom/issues/2524#issuecomment-736672511
 class RedwoodWebJestEnvironment extends TestEnvironment {
-  async setup(): Promise<void> {
+  async setup() {
     await super.setup()
     if (typeof this.global.TextEncoder === 'undefined') {
       this.global.TextEncoder = TextEncoder
-      this.global.TextDecoder = TextDecoder as any
+      // @ts-expect-error - TextDecoder from node:utils is close enough
+      this.global.TextDecoder = TextDecoder
     }
     if (typeof this.global.crypto.subtle === 'undefined') {
       // @ts-expect-error - We're just making sure the object is there to make
