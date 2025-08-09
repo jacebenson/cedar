b2ae7bce01c0d5b97da3c87893d0915e01bb57ac
Tobbe Lundberg <tobbe@tlundberg.com>
2025-08-09 11:04:28 +0200

    test db import tracking

---
+++ tasks/test-project/templates/api/3-db-import.test.ts
@@ -0,0 +1,5 @@
+test('Cedar is correctly tracking db imports 1', () => {
+  // The previous test (2-db-import.test.ts) imported the database but this one
+  // doesn't, so __cedarjs_db_imported__ should be falsy again
+  expect(globalThis.__cedarjs_db_imported__).toBeFalsy()
+})
