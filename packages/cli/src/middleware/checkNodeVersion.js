d3750c273ac0700d00b17a9d147f5445df9d9fb8
Tobbe Lundberg <tobbe@tlundberg.com>
2025-11-03 14:56:16 +0100

    fix(cli): Fix spacing in node version check message

--- packages/cli/src/middleware/checkNodeVersion.js
+++ packages/cli/src/middleware/checkNodeVersion.js
@@ -17,7 +17,7 @@ export function checkNodeVersion() {
   checks.ok = false
   checks.message = [
     `Your Node.js version is ${c.warning(pVersion)}, but Cedar requires ` +
-      `${c.important(`>=${LOWER_BOUND}`)}.`,
+      `${c.important(`>= ${LOWER_BOUND}`)}.`,
     'Upgrade your Node.js version using `nvm`, `n`, or a similar tool. See ' +
       'https://cedarjs.com/docs/how-to/using-nvm.',
   ].join('\n')
